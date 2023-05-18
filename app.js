668b220cc88eb52c1160c2b76814831b23f3ddb0
Mayowa-jerrie <108583250+Mayowa-jerrie@users.noreply.github.com>
2023-05-18 17:50:12 +0100

    js changes

--- app.js
+++ app.js
@@ -3,4 +3,24 @@ let navbar = document.querySelector(".navbar");
 
 
 menu.onclick = () =>{
-menu.classList.toggle("fa-times");}
+menu.classList.toggle("fa-times");
+navbar.classList.toggle("active");}
+
+window.onscroll = () =>{
+
+    if(window.scrollY > 0){
+        document.querySelector(".header").classList.add("active");
+    }else{
+        document.querySelector(".header").classList.remove("active");
+    }
+    menu.classList.remove("fa-times");
+    navbar.classList.remove("active")};
+
+    window.onload = () =>{
+
+        if(window.scrollY > 0){
+            document.querySelector(".header").classList.add("active");
+        }else{
+            document.querySelector(".header").classList.remove("active");
+        }
+    };
