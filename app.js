dfee87592d7d0376deae887fc71568e6e40ade7b
Mayowa-jerrie <108583250+Mayowa-jerrie@users.noreply.github.com>
2023-05-19 09:15:00 +0100

    continued application

--- app.js
+++ app.js
@@ -6,6 +6,17 @@ menu.onclick = () =>{
 menu.classList.toggle("fa-times");
 navbar.classList.toggle("active");}
 
+document.querySelector("#login-btn").onclick = () =>{
+    document.querySelector(".login-form-container").classList.toggle("active");
+} 
+
+document.querySelector("#close-login-form").onclick = () =>{
+    document.querySelector(".login-form-container").classList.remove("active");
+} 
+
+
+
+
 window.onscroll = () =>{
 
     if(window.scrollY > 0){
@@ -24,3 +35,25 @@ window.onscroll = () =>{
             document.querySelector(".header").classList.remove("active");
         }
     };
+    document.querySelector(".home").onmousemove = (e) =>{
+        document.querySelectorAll(".home-parallax").forEach(elm =>{
+
+            let speed = elm.getAttribute("data-speed");
+            let x = (window.innerWidth - e.pageX * speed) / 90;
+            let y = (window.innerHeight - e.pageY * speed) / 90;
+
+            elm.style.transform = `translateX(${y}px) translateY(${x}px)`;
+
+        });
+    }
+
+    document.querySelector(".home").onmouseleave = () =>{
+
+        document.querySelectorAll(".home-parallax").forEach(elm =>{
+
+             
+
+            elm.style.transform = `translateX(0px) translateY(0px)`;
+
+        });
+    }
